bd75101b84fc569db8df82ace4cf19be6efc129d
hungdinh125 <73471020+hungdinh125@users.noreply.github.com>
2023-10-17 07:55:38 +0000

    edit and add subnet

--- vpc.tf
+++ vpc.tf
@@ -9,6 +9,6 @@ resource "aws_subnet" "subnet1" {
     vpc_id = aws_vpc.dean_test_vpc.id
     cidr_block = "10.10.1.0/24"
     tags = {
-        Name = var.tags
+        Name = "subnet_workflow"
     }
 }
